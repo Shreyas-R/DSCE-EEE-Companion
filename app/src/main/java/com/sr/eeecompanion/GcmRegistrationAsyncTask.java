04fdb282860f648f0fa3b9061a44531d6f16b2d6
Shreyas R <shreyasramesh81@gmail.com>
2016-03-07 17:50:52 +0530

    Adding Login support

---
+++ app/src/main/java/com/sr/eeecompanion/GcmRegistrationAsyncTask.java
@@ -0,0 +1,69 @@
+package com.sr.eeecompanion;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.sr.eeecompanion.backend.registration.Registration;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by Shreyas R on 07-03-2016.
+ */
+public class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
+    private static Registration regService = null;
+    private GoogleCloudMessaging gcm;
+    private Context context;
+
+    private static final String SENDER_ID = "821940351127";
+
+    public GcmRegistrationAsyncTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        if (regService == null) {
+            Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://rosy-pivot-124309.appspot.com/_ah/api/");
+
+            regService = builder.build();
+        }
+
+        String msg = "";
+        try {
+            if (gcm == null) {
+                gcm = GoogleCloudMessaging.getInstance(context);
+            }
+            String regId = gcm.register(SENDER_ID);
+            msg = "Device registered, registration ID=" + regId;
+
+            // You should send the registration ID to your server over HTTP,
+            // so it can use GCM/HTTP or CCS to send messages to your app.
+            // The request to your server should be authenticated if your app
+            // is using accounts.
+            regService.register(regId).execute();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            msg = "Error: " + ex.getMessage();
+        }
+        return msg;
+    }
+
+    @Override
+    protected void onPostExecute(String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+        Logger.getLogger("REGISTRATION").log(Level.INFO, msg);
+    }
+
+
+}
